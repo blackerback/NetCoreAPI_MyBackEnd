1166a003e755bfe5bdf9aa26ecd00adfdd815442
blackerback <66015729+blackerback@users.noreply.github.com>
2020-05-27 15:39:27 +0300

    Update UserForRegisterDto.cs

--- MyBackEnd.Entities/DTO/UserForRegisterDto.cs
+++ MyBackEnd.Entities/DTO/UserForRegisterDto.cs
@@ -7,6 +7,7 @@ namespace MyBackEnd.Entities.DTO
 {
     public class UserForRegisterDto:IDto
     {
+    
         public string Email { get; set; }
         public string Password { get; set; }
         public string FirstName { get; set; }
